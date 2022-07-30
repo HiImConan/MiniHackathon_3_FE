cbb4016310230ca0a6faa70082c3d46a524e9cfa
HiImConan <mindol0320@naver.com>
2022-07-30 16:04:21 +0900

    feat: prevent api limit problem

---
+++ hk_fe_3/src/styles/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle, css } from "styled-components";
+import reset from "styled-reset";
+import customReset from "./customReset.scss";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    ${customReset};
+
+
+    ${({ theme }) => {
+      return css`
+        body {
+          font-family: ${theme.fonts.family.base};
+          font-weight: ${theme.fonts.weight.normal};
+          font-size: ${theme.fonts.size.base};
+        }
+      `;
+    }}
+`;
+
+export default GlobalStyle;
