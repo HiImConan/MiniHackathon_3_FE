53562fa2d001e6526b185163a05e374560a0807c
HiImConan <mindol0320@naver.com>
2022-07-30 13:54:59 +0900

    feat: design rough layout

--- hk_fe_3/src/assets/Api_test.js
+++ hk_fe_3/src/assets/Api_test.js
@@ -1,23 +1,25 @@
+import axios from "axios";
+
 export async function getMovieApi() {
-  const res = await fetch(
+  const res = await axios.get(
     `https://d0cc7814-98ab-422a-b0ef-82d4165b2542.mock.pstmn.io/`
   );
 
-  return await res.json();
+  return res;
 }
 
 export async function getDetailApi() {
-  const res = await fetch(
+  const res = await axios.get(
     `https://d0cc7814-98ab-422a-b0ef-82d4165b2542.mock.pstmn.io/movie/2`
   );
 
-  return await res.json();
+  return res;
 }
 
 export async function getCommentApi() {
-  const res = await fetch(
+  const res = await axios.get(
     `ttps://d0cc7814-98ab-422a-b0ef-82d4165b2542.mock.pstmn.io/movie/comments/2`
   );
 
-  return await res.json();
+  return res;
 }
